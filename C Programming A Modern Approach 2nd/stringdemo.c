97e1a58effcc3844871898859b97b5e903618671
lanhuidong <lanhuidong@gmail.com>
2016-07-02 21:35:36 +0800

    add stringdemo

---
+++ C Programming A Modern Approach 2nd/stringdemo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+    /*如果两个字符串之间只有空白字符，编译器会将两个字符串连接到一起*/
+    printf("Hello "
+    "world!\n");
+    
+    printf("Hello \0 world!");
+    printf("\n");
+    return 0;
+}
