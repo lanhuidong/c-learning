648fbdca44be307ccebc7bdfd34f9c875d6d0b3b
lanhuidong <lanhuidong@gmail.com>
2017-10-31 14:16:53 +0800

    读取内存信息

--- dog/dog.c
+++ dog/dog.c
@@ -8,7 +8,18 @@ int main(int argc, char *argv[])
     {
         unsigned long total_space = vfs.f_bsize * vfs.f_blocks;
         unsigned long free_space = vfs.f_bsize * vfs.f_bavail;
-        printf("total disk space: %lu\n", total_space / 1024 / 1024);
-        printf("free disk space: %lu\n", free_space / 1024 / 1024);
+        printf("total disk space: %lu\n", total_space / 1024);
+        printf("free disk space: %lu\n", free_space / 1024);
+
+        FILE *fp;
+	unsigned long total_mem;
+        unsigned long free_mem;
+	if((fp = fopen("/proc/meminfo", "r")) != NULL){
+            fscanf(fp, "MemTotal: %lu kB\n", &total_mem);
+            fscanf(fp, "MemFree: %lu kB\n", &free_mem);
+	    printf("MemTotal: %lu kB\n", total_mem);
+	    printf("MemFree: %lu kB\n", free_mem);
+	}
+	fclose(fp);
     }
 }
